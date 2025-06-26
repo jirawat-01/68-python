75978dcd9da30740e1f1ac8a94835dc40c2c46c7
jirawat-01 <pose11490@gmail.com>
2025-06-26 17:02:43 +0700

    End Lecture-1

---
+++ lecture-01/testnumeic.py
@@ -0,0 +1,11 @@
+from math import pi
+print('%d'  %100)
+print('%d'  %100.58)
+print('%f'  %100.58)
+print('%.2f'   %100.58)
+print('%.4f'    %pi)
+print('%.50f'   %pi)
+print('my   age is  %d  i   have    %2.f    bth.'   %(25,3500.50))
+print('my age is '+ str(25)+'   I have  '+ str(3500)+   'bth.')
+
+print('5+4=',5+4)
