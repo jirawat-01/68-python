3dedeef3685e1e42b8a3659074838ac15b9843e4
jirawat-01 <pose11490@gmail.com>
2025-06-26 16:37:51 +0700

    FirstCommit

---
+++ lecture-01/testpint.py
@@ -0,0 +1,9 @@
+print("Hello")
+print("jirawat  chrdsungnran")
+print("Im`hungry")
+print("I`m  good and\"I will do mybest\"")
+print('/\\')
+print('''\ta\ta
+      a\ta\ta\ta\ta
+      \ta\ta\ta
+      \ta''')
